d077e0202a1a8d18ca85b7bfa75f9b7be53e0362
Maxim Ivanov <maxim_91@inbox.ru>
2016-01-24 14:56:33 +0300

    The first page is done

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require("gulp"),
-	browserSync = require('browser-sync');
 
+	browserSync = require('browser-sync'),
+	autoprefixer = require('gulp-autoprefixer'),
+    spritesmith = require('gulp.spritesmith'),
+	minifyCSS = require('gulp-minify-css'),
+	rename = require('gulp-rename'),
+	concatCss = require('gulp-concat-css');
 //Сервер
 gulp.task('server',function(){
 		browserSync({
@@ -20,3 +25,19 @@ gulp.task ('watch',function(){
 		});
 //Задача по умолчанию
 gulp.task('default',['server','watch']);
+
+gulp.task('sprite', function () {
+  var spriteData = gulp.src('app/img/i-*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: 'sprite.css'
+  }));
+  return spriteData.pipe(gulp.dest('app/sprites/'));
+});
+
+ gulp.task('autoprefixer', function(){
+    gulp.src('css/style.css')
+    .pipe(autoprefixer({
+      browsers: ['> 1%', 'last 2 versions','ie 8']
+    }))
+    .pipe(gulp.dest('app/css/*.css'));
+  });
